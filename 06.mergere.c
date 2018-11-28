fce197af125c6f63874d0d4ef1f4295e7d14fc6d
ceccs17d10 <43630401+ceccs17d10@users.noreply.github.com>
2018-11-29 00:13:52 +0530

    Update 06.mergere.c

--- 06.mergere.c
+++ 06.mergere.c
@@ -42,4 +42,21 @@ void merge(int a[],int i1,int j1,int i2,int j2)
      for(i=i1,j=0;i<=j2;i++,j++)
 	   a[i]=temp[j];
 }
+/*OUTPUT
+Enter no: of elements:5
+Enter array elements:3
+4
+52
+3
+4
+Sorted Array:
+3
+
+3
+
+4
+
+4
+
+52
 
