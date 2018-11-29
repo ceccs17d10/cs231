2f3a1d0b035bc09ddf899a246d9982d4dd695367
ceccs17d10 <43630401+ceccs17d10@users.noreply.github.com>
2018-11-29 14:18:43 +0530

    Update 28.circularqueueusingarray.c

--- 28.circularqueueusingarray.c
+++ 28.circularqueueusingarray.c
@@ -1,129 +1,129 @@
-/* Roll no 10
-*ANUROSE K THOMAS
-*Prg NO 28
-*Implementation of circular queue using array
-*/
-#include<stdio.h>
-#include<string.h>
+        /* Roll no 10
+        *ANUROSE K THOMAS
+        *Prg NO 28
+        *Implementation of circular queue using array
+        */
+        #include<stdio.h>
+        #include<string.h>
 
-void main()
-{
-    int arr[11],f,l,i,ele,temp,choice;
-    f=0;
-    l=0;
-
-    do
-    {
-        printf("\nMENU\n1.Insertion\n2.Deletion\n3.Dispaly\n4.Exit\nEnter choice: ");
-        scanf("%d",&choice);
-        switch(choice)
+        void main()
         {
-            case 1: if(l==0)
-                    {
-                        printf("Enter element: ");
-                        scanf("%d",&ele);
-                        f=1;
-                        l=1;
-                        arr[l]=ele;
+            int arr[11],f,l,i,ele,temp,choice;
+            f=0;
+            l=0;
 
-                    }
-                    else if(f==(l+1))
-                    {
-                        printf("\nQueue is FULL\n");
-                    }
-                    else
-                    {
-                        temp=l;
-                        temp=((temp%10)+1);
-                        if(temp!=f)
-                        {
-                            l=temp;
-                            printf("Enter element: ");
-                            scanf("%d",&ele);
-                            arr[l]=ele;
-                        }
-                        else
-                        {
-                            printf("\nQueue is FULL\n");
-                        }
-                    }
-                    break;
-            case 2: if(f==0)
-                    {
-                        printf("\nQueue is EMPTY\n");
-                    }
-                    else
-                    {
-                        if(f==l)
-                        {
-                            f=0;
-                            l=0;
-                        }
-                        else
-                        {
-                            f=((f%10)+1);
+            do
+            {
+                printf("\nMENU\n1.Insertion\n2.Deletion\n3.Dispaly\n4.Exit\nEnter choice: ");
+                scanf("%d",&choice);
+                switch(choice)
+                {
+                    case 1: if(l==0)
+                            {
+                                printf("Enter element: ");
+                                scanf("%d",&ele);
+                                f=1;
+                                l=1;
+                                arr[l]=ele;
 
-                        }
-                    }
-                    break;
-            case 3: i=f;
-                    while(i!=(l+1))
-                    {
-                        if(l==10 && i==10)
-                        {
-                            printf(" %d <-",arr[i]);
+                            }
+                            else if(f==(l+1))
+                            {
+                                printf("\nQueue is FULL\n");
+                            }
+                            else
+                            {
+                                temp=l;
+                                temp=((temp%10)+1);
+                                if(temp!=f)
+                                {
+                                    l=temp;
+                                    printf("Enter element: ");
+                                    scanf("%d",&ele);
+                                    arr[l]=ele;
+                                }
+                                else
+                                {
+                                    printf("\nQueue is FULL\n");
+                                }
+                            }
                             break;
-                        }
-                        printf(" %d <-",arr[i]);
-                        i=((i%10)+1);
-                    }
-                    break;
+                    case 2: if(f==0)
+                            {
+                                printf("\nQueue is EMPTY\n");
+                            }
+                            else
+                            {
+                                if(f==l)
+                                {
+                                    f=0;
+                                    l=0;
+                                }
+                                else
+                                {
+                                    f=((f%10)+1);
+
+                                }
+                            }
+                            break;
+                    case 3: i=f;
+                            while(i!=(l+1))
+                            {
+                                if(l==10 && i==10)
+                                {
+                                    printf(" %d <-",arr[i]);
+                                    break;
+                                }
+                                printf(" %d <-",arr[i]);
+                                i=((i%10)+1);
+                            }
+                            break;
+                }
+
+            }while(choice==1 || choice==2 || choice==3);
         }
-        
-    }while(choice==1 || choice==2 || choice==3);
-}
-/*OUTPUT
-MENU
-1.Insertion
-2.Deletion
-3.Dispaly
-4.Exit
-Enter choice: 1
-Enter element: 23
+        /*OUTPUT
+        MENU
+        1.Insertion
+        2.Deletion
+        3.Dispaly
+        4.Exit
+        Enter choice: 1
+        Enter element: 23
 
-MENU
-1.Insertion
-2.Deletion
-3.Dispaly
-4.Exit
-Enter choice: 1
-Enter element: 4
+        MENU
+        1.Insertion
+        2.Deletion
+        3.Dispaly
+        4.Exit
+        Enter choice: 1
+        Enter element: 4
 
-MENU
-1.Insertion
-2.Deletion
-3.Dispaly
-4.Exit
-Enter choice: 3
- 23 <- 4 <-
-MENU
-1.Insertion
-2.Deletion
-3.Dispaly
-4.Exit
-Enter choice: 2
+        MENU
+        1.Insertion
+        2.Deletion
+        3.Dispaly
+        4.Exit
+        Enter choice: 3
+         23 <- 4 <-
+        MENU
+        1.Insertion
+        2.Deletion
+        3.Dispaly
+        4.Exit
+        Enter choice: 2
 
-MENU
-1.Insertion
-2.Deletion
-3.Dispaly
-4.Exit
-Enter choice: 3
- 4 <-
-MENU
-1.Insertion
-2.Deletion
-3.Dispaly
-4.Exit
-Enter choice: 3
+        MENU
+        1.Insertion
+        2.Deletion
+        3.Dispaly
+        4.Exit
+        Enter choice: 3
+         4 <-
+        MENU
+        1.Insertion
+        2.Deletion
+        3.Dispaly
+        4.Exit
+        Enter choice: 3
 
