d89009c832f306d74df9e78470720cf75235db71
ceccs17d10 <43630401+ceccs17d10@users.noreply.github.com>
2018-11-29 13:53:41 +0530

    Update 02.selectionsort.c

--- 02.selectionsort.c
+++ 02.selectionsort.c
@@ -1,40 +1,40 @@
-/* Roll no 10
-* ANUROSE K THOMAS
-*PRG NO 02
-*Selection sort 
-*/
- 
+	/* Roll no 10
+	* ANUROSE K THOMAS
+	*PRG NO 02
+	*Selection sort 
+	*/
 
-#include<stdio.h>
-int main()
-{
-	int i,j,c,temp,n[25];
-	printf("enter the size");
-   	scanf("%d",&c);
-	printf("Enter elements:");
-  	for(i=0;i<count;i++)
-      		scanf("%d",&n[i]);
- 	for(i=0;i<c;i++)
+
+	#include<stdio.h>
+	int main()
 	{
-      		for(j=i+1;j<c;j++)
+		int i,j,c,temp,n[25];
+		printf("enter the size");
+		scanf("%d",&c);
+		printf("Enter elements:");
+		for(i=0;i<count;i++)
+			scanf("%d",&n[i]);
+		for(i=0;i<c;i++)
 		{
-         		if(n[i]>n[j])
+			for(j=i+1;j<c;j++)
 			{
-            			temp=n[i];
-            			n[i]=n[j];
-            			n[j]=temp;
-         		}
-      		}
-   	}
-	printf("Sorted elements: ");
-   	for(i=0;i<c;i++)
-      	printf(" %d",n[i]);
-	return 0;
-}
-/*OUTPUT
-enter the size4
-Enter elements:3
-5
-2
-1
-Sorted elements:  1 2 3 5
+				if(n[i]>n[j])
+				{
+					temp=n[i];
+					n[i]=n[j];
+					n[j]=temp;
+				}
+			}
+		}
+		printf("Sorted elements: ");
+		for(i=0;i<c;i++)
+		printf(" %d",n[i]);
+		return 0;
+	}
+	/*OUTPUT
+	enter the size4
+	Enter elements:3
+	5
+	2
+	1
+	Sorted elements:  1 2 3 5
