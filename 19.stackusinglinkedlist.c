1550dd93999b9593170803ced978e2ba41711ae0
ceccs17d10 <43630401+ceccs17d10@users.noreply.github.com>
2018-11-29 14:10:01 +0530

    Update 19.stackusinglinkedlist.c

--- 19.stackusinglinkedlist.c
+++ 19.stackusinglinkedlist.c
@@ -1,114 +1,114 @@
-/*ANUROSE KTHOMAS
-ROLL NO 10
-PRG NO 19
-IMPLEMENTATION OF STACK USING LINKED LIST
-*/
+        /*ANUROSE KTHOMAS
+        ROLL NO 10
+        PRG NO 19
+        IMPLEMENTATION OF STACK USING LINKED LIST
+        */
 
-#include<stdio.h>
-#include<stdlib.h>
+        #include<stdio.h>
+        #include<stdlib.h>
 
-struct node
-{
-    int data;
-    struct node *link;
-};
-
-void main()
-{
-    int cho;
-    struct node *temp,*top,*head;
-    temp=(struct node*)malloc(sizeof(struct node));
-    temp->data=NULL;
-    temp->link=NULL;
-    head=temp;
-    top=head->link;
-    do
-    {
-        
-        printf("\nMENU\n1.PUSH\n2.POP\n3.DISPLAY\n4.EXIT\nEnter Choice: ");
-        scanf("%d",&cho);
-
-        if(cho==1)
+        struct node
         {
+            int data;
+            struct node *link;
+        };
+
+        void main()
+        {
+            int cho;
+            struct node *temp,*top,*head;
             temp=(struct node*)malloc(sizeof(struct node));
-            printf("enter the element: ");
-            scanf("%d",&temp->data);
-            temp->link=head->link;
-            head->link=temp;
+            temp->data=NULL;
+            temp->link=NULL;
+            head=temp;
             top=head->link;
-        }
-        else if(cho==2)
-        {
-            if(top==NULL)
+            do
             {
-                printf("stack is empty!!!");
-            }
-            else
-            {
-                printf("popped out: %d",top->data);
-                temp=top;
-                top=top->link;
-                head->link=top;
-                free(temp);
-            }
+
+                printf("\nMENU\n1.PUSH\n2.POP\n3.DISPLAY\n4.EXIT\nEnter Choice: ");
+                scanf("%d",&cho);
+
+                if(cho==1)
+                {
+                    temp=(struct node*)malloc(sizeof(struct node));
+                    printf("enter the element: ");
+                    scanf("%d",&temp->data);
+                    temp->link=head->link;
+                    head->link=temp;
+                    top=head->link;
+                }
+                else if(cho==2)
+                {
+                    if(top==NULL)
+                    {
+                        printf("stack is empty!!!");
+                    }
+                    else
+                    {
+                        printf("popped out: %d",top->data);
+                        temp=top;
+                        top=top->link;
+                        head->link=top;
+                        free(temp);
+                    }
+                }
+
+                else if(cho==3)
+                {
+                    printf("elements: ");
+                    temp=top;
+                    while(temp!=NULL)
+                    {
+                        printf("%d ->",temp->data);
+                        temp=temp->link;
+                    }
+                    printf("NULL");
+                }
+
+            }while(cho==1 || cho==2 || cho==3);
+
         }
+        /* OUTPUT
+        MENU
+        1.PUSH
+        2.POP
+        3.DISPLAY
+        4.EXIT
+        Enter Choice: 1
+        enter the element: 2
 
-        else if(cho==3)
-        {
-            printf("elements: ");
-            temp=top;
-            while(temp!=NULL)
-            {
-                printf("%d ->",temp->data);
-                temp=temp->link;
-            }
-            printf("NULL");
-        }
-        
-    }while(cho==1 || cho==2 || cho==3);
-    
-}
-/* OUTPUT
-MENU
-1.PUSH
-2.POP
-3.DISPLAY
-4.EXIT
-Enter Choice: 1
-enter the element: 2
+        MENU
+        1.PUSH
+        2.POP
+        3.DISPLAY
+        4.EXIT
+        Enter Choice: 1
+        enter the element: 4
 
-MENU
-1.PUSH
-2.POP
-3.DISPLAY
-4.EXIT
-Enter Choice: 1
-enter the element: 4
-
-MENU
-1.PUSH
-2.POP
-3.DISPLAY
-4.EXIT
-Enter Choice: 3
-elements: 4 ->2 ->NULL
-MENU
-1.PUSH
-2.POP
-3.DISPLAY
-4.EXIT
-Enter Choice: 2
-popped out: 4
-MENU
-1.PUSH
-2.POP
-3.DISPLAY
-4.EXIT
-Enter Choice: 3
-elements: 2 ->NULL
-MENU
-1.PUSH
-2.POP
-3.DISPLAY
-4.EXIT
-Enter Choice: 4
+        MENU
+        1.PUSH
+        2.POP
+        3.DISPLAY
+        4.EXIT
+        Enter Choice: 3
+        elements: 4 ->2 ->NULL
+        MENU
+        1.PUSH
+        2.POP
+        3.DISPLAY
+        4.EXIT
+        Enter Choice: 2
+        popped out: 4
+        MENU
+        1.PUSH
+        2.POP
+        3.DISPLAY
+        4.EXIT
+        Enter Choice: 3
+        elements: 2 ->NULL
+        MENU
+        1.PUSH
+        2.POP
+        3.DISPLAY
+        4.EXIT
+        Enter Choice: 4
